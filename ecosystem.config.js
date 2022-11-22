c27b604d2b4c5cbf68368897302fc85dfadafc40
Romel Niño Paano <romelpaano@10.100.28.182>
2022-11-23 00:55:24 +0800

    Add pm2 ecosystem

---
+++ ecosystem.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  apps : [{
+    script: 'index.js',
+  },],
+
+  deploy : {
+    production : {
+      key  : '~/.cert/key.cer',
+      user : 'ubuntu',
+      host : '13.212.196.162',
+      ref  : 'origin/master',
+      repo : 'git@github.com:rpaano/NewProjectNuxt.git',
+      path : 'home/ubuntu',
+      'pre-deploy-local': '',
+      'post-deploy' : 'source ~/.nvm/nvm.sh && npm install && npm run build && npm install && pm2 reload ecosystem.config.js --env production',
+      'pre-setup': '',
+      'ssh_options': 'ForwardAgent=yes'
+    }
+  }
+};
