320140a0d8137e8ca9908f74f3b38f317d6229ca
Romel Niño Paano <romelpaano@10.100.28.182>
2022-11-23 01:39:06 +0800

    Change directory

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -10,7 +10,7 @@ module.exports = {
       host : '13.212.196.162',
       ref  : 'origin/master',
       repo : 'git@github.com:rpaano/NewProjectNuxt.git',
-      path : 'home/ubuntu',
+      path : '/home/ubuntu',
       'pre-deploy-local': '',
       'post-deploy' : 'source ~/.nvm/nvm.sh && npm install && npm run build && npm install && pm2 reload ecosystem.config.js --env production',
       'pre-setup': '',
